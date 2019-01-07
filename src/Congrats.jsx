589fd670450681bfe4ed385f377bf0190b9127ee
rifkegribenes <rifkegribenes@gmail.com>
2019-01-06 22:16:58 -0800

    configure enzyme, test boilerplate

---
+++ src/Congrats.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * Functional React component for congrats message
+ * @function
+ * @returns {JSX.Element} Rendered component (or null if 'success' prop is false)
+ */
+
+
+export default () => {
+	return (
+		<div/>
+		)
+}
