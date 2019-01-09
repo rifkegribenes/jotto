772a7bca673641e5a3434b112c086c42af593d25
rifkegribenes <rifkegribenes@gmail.com>
2019-01-08 22:23:17 -0800

    add checkProps test"

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,6 @@
 
+import checkPropTypes from 'check-prop-types';
+
 /**
  * Return node(s) with the given data-test attribute.
  * @param  {ShallowWrapper} wrapper - Enzyme shallow wrapper.
@@ -7,4 +9,13 @@
  */
 export const findByTestAttr = (wrapper, val) => {
 	return wrapper.find(`[data-test="${val}"]`);
+}
+
+export const checkProps = (component, conformingProps) => {
+	const propError = checkPropTypes(
+		component.PropTypes,
+		conformingProps,
+		'prop',
+		component.name);
+	expect(propError).toBeUndefined();
 }
