0e9797c4685808f67f3459cfa6e6e0a9585ddb2a
rifkegribenes <rifkegribenes@gmail.com>
2019-01-10 22:35:48 -0800

    tests and code for non-zero guessed words

--- src/GuessedWords.jsx
+++ src/GuessedWords.jsx
@@ -8,7 +8,38 @@ const GuessedWords = (props) => {
 			<span data-test="guess-instructions">
 				Try to guess the secret word!s
 			</span>
-			);
+		);
+	} else {
+		const guessedWordsRows = props.guessedWords.map((word, index) => (
+				<tr key={index} data-test="guessed-word">
+					<td>
+						{word.guessedWord}
+					</td>
+					<td>
+						{word.letterMatchCount}
+					</td>
+				</tr>
+			))
+		contents = (
+			<div data-test="guessed-words">
+				<h3>GuessedWords</h3>
+				<table>
+					<thead>
+						<tr>
+							<th>
+								Guess
+							</th>
+							<th>
+								Matching Letters
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{ guessedWordsRows }
+					</tbody>
+				</table>
+			</div>
+		);
 	}
 
 	return (
