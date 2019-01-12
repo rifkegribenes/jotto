92bc1d68919a16e507157feeae405435ecc5c7a7
rifkegribenes <rifkegribenes@gmail.com>
2019-01-11 17:18:08 -0800

    setup redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function successReducer
+ * @param  {array} state  - Array of guessed words.
+ * @param  {object} action - Object to be reduced.
+ * @return {boolean}        New success state.
+ */
+export default (state, action) => {
+	return null;
+}
