bfeb8aa7e6f6d5437ce337fa89bf7ea646d6a232
rifkegribenes <rifkegribenes@gmail.com>
2019-01-13 13:23:57 -0800

    added redux thunk

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,16 +1,22 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
+
+
+// storeFactory function is unused / unusable now that react-redux 6.x
+// deprecates passing store directly to component
 /**
  * Create a testing store with imported reducers, initial state, and middleware.
- * globals: rootReducer.
+ * globals: rootReducer, middlewares
  * @param  {object} initialState - Initial state for the store.
  * @function  storeFactory
  * @return {Store}              - Redux store.
  */
 export const storeFactory = (initialState) => {
-	return createStore(rootReducer, initialState);
+	const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+	return createStoreWithMiddleware(rootReducer, initialState);
 }
 
 /**
